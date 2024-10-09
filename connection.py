f469bbbd466f575db0a5418e57d135ae59fd5a1a
Artyom Korobkin <carlionminon@gmail.com>
2024-10-09 17:02:22 +0300

    UPD: SRP

--- connection.py
+++ connection.py
@@ -5,7 +5,6 @@ from scrapper import Scrap
 class SQLConnect:
     """
     Класс SQLConnect отвечает за управление данными пользователей и взаимодействие с базой данных SQLite.
-    Он также включает методы для создания ссылки поиска на основе фильтров пользователей и парсинга данных вакансий.
     """
 
     def __init__(self):
@@ -62,7 +61,7 @@ class SQLConnect:
             temp = self.cursor.execute("SELECT include, exclude, region, salary, experience, education, employment, schedule, part_time FROM users WHERE username = ?", (username,))
             return temp.fetchall()
         except sql.Error as e:
-            return {'ошибка при получении фильтров': e}
+            return {'Ошибка при получении фильтров': e}
 
     def add_filters(self, username, include, exclude, region, salary, experience, education, employment, schedule, part_time):
         """
@@ -77,57 +76,22 @@ class SQLConnect:
             self.connect.commit()
             return True
         except sql.Error as e:
-            return {'ошибка при добавлении фильтров': e}
+            return {'Ошибка при добавлении фильтров': e}
 
-    def generate_link(self, filters) -> str:
-        """
-        Создает ссылку поиска на сайте HH.ru на основе предоставленных фильтров.
-        Возвращает сформированную ссылку в виде строки.
-        """
-        base_url = "https://hh.ru/search/vacancy?"
-        params = {
-            "text": filters[0],          
-            "excluded_text": filters[1],  
-            "area": filters[2],           
-            "salary": filters[3],         
-            "experience": filters[4],     
-            "education": filters[5],      
-            "employment": filters[6],     
-            "schedule": filters[7],       
-            "part_time": filters[8],     
-            "L_save_area": "true",        
-            "order_by": "relevance",      
-            "items_on_page": 50          
-        }
-        
-        query_string = "&".join([f"{key}={value}" for key, value in params.items() if value])
-        result_url = base_url + query_string
-        return result_url
-
-    def truncate_string(input_string: str, word_limit=50) -> str:
-        """
-        Обрезает входную строку до указанного количества слов.
-        Если количество слов во входной строке превышает лимит, обрезает ее и добавляет многоточие.
-        Возвращает обрезанную строку.
-        """
-        words = input_string.split()  
-        if len(words) > word_limit:
-            truncated = " ".join(words[:word_limit]) + "..."  
-            return truncated
-        return input_string 
+     
 
     def search(self, username) -> list:
         """
         Выполняет поиск вакансий на сайте HH.ru на основе фильтров пользователя.
         Возвращает список найденных вакансий.
         """
-        filters = self.get_filters(username)
-        url = self.generate_link(filters)
+        filters = self.scrap.get_filters(username)
+        url = self.scrap.generate_link(filters)
         self.scrap.add_url(url)
         vacancies = self.scrap.parse()
         return vacancies
 
-    def generate_message(self, vacancies) -> list:
+    def generate_message(self, vacancies) -> tuple:
         """
         Формирует сообщение о вакансии для отправки пользователю.
         Возвращает кортеж из сообщения о вакансии и URL вакансии.
--- scrapper.py
+++ scrapper.py
@@ -37,7 +37,42 @@ class Scrap():
         Инициализирует экземпляр класса Scrap.
         """
         pass
+    def generate_link(self, filters) -> str:
+        """
+        Создает ссылку поиска на сайте HH.ru на основе предоставленных фильтров.
+        Возвращает сформированную ссылку в виде строки.
+        """
+        base_url = "https://hh.ru/search/vacancy?"
+        params = {
+            "text": filters[0],          
+            "excluded_text": filters[1],  
+            "area": filters[2],           
+            "salary": filters[3],         
+            "experience": filters[4],     
+            "education": filters[5],      
+            "employment": filters[6],     
+            "schedule": filters[7],       
+            "part_time": filters[8],     
+            "L_save_area": "true",        
+            "order_by": "relevance",      
+            "items_on_page": 50          
+        }
+        
+        query_string = "&".join([f"{key}={value}" for key, value in params.items() if value])
+        result_url = base_url + query_string
+        return result_url
 
+    def truncate_string(input_string: str, word_limit=50) -> str:
+        """
+        Обрезает входную строку до указанного количества слов.
+        Если количество слов во входной строке превышает лимит, обрезает ее и добавляет многоточие.
+        Возвращает обрезанную строку.
+        """
+        words = input_string.split()  
+        if len(words) > word_limit:
+            truncated = " ".join(words[:word_limit]) + "..."  
+            return truncated
+        return input_string
     def add_url(self, url: str):
         """
         Добавляет URL в экземпляр Scrap.
