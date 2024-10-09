0890ab90c5e9393df089fa136b6afa80f9946a08
Artyom Korobkin <carlionminon@gmail.com>
2024-10-09 16:23:19 +0300

    ADD: Documentation fkr scrapper

--- connection.py
+++ connection.py
@@ -1,7 +1,11 @@
 import sqlite3 as sql
+from scrapper import Scrap
+
+
 #https://hh.ru/search/vacancy?L_save_area=true&text=aaaa&excluded_text=aaaaaa&area=1002&salary=123124&currency_code=RUR&education=not_required_or_not_specified&experience=between1And3&employment=full&schedule=fullDay&part_time=employment_project&label=with_address&order_by=relevance&search_period=0&items_on_page=50&hhtmFrom=vacancy_search_filter
 class SQLConnect():
     def __init__(self):
+        self.scrap = Scrap()
         self.connect = sql.connection('base/db.sqlite3', check_same_thread=False)
         self.cursor = self.connect.cursor()
         self.cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TINYTEXT, include TEXT, exclude TEXT, region INTEGER, salary INTEGER, experience TEXT, education TEXT, employment TEXT, schedule TEXT, part_time TEXT)")
@@ -33,6 +37,14 @@ class SQLConnect():
             return True
         except sql.Error as e:
             return {'error at adding filters': e}
+    def generate_link(self, **kwargs):
+        pass
     def search(self, username):
         filters = self.get_filters(username)
-        
+        url = self.generate_link(filters)
+        self.scrap.add_url(url)
+        vacancies = self.scrap.parse()
+        return vacancies
+    def generate_message(self, vacancies):
+        for vacancy in vacancies:
+            pass
--- scrapper.py
+++ scrapper.py
@@ -1,16 +1,25 @@
 from bs4 import BeautifulSoup
 import lxml
 
-#На странице поиска вакансий
-#data-sentry-element="MagritteLink" - ссылка на вакансию
-#На странице вакансии
-#data-sentry-source-file="VacancyTitleRedesigned.tsx" - название вакансии
-#data-qa="vacancy-description" - описание вакансии 
-#data-qa="vacancy-salary-compensation-type-gross" - зп по вакансии
+# На странице поиска вакансий
+# data-sentry-element="MagritteLink" - ссылка на вакансию
+# На странице вакансии
+# data-sentry-source-file="VacancyTitleRedesigned.tsx" - название вакансии
+# data-qa="vacancy-description" - описание вакансии 
+# data-qa="vacancy-salary-compensation-type-gross" - зп по вакансии
 
 from bs4 import BeautifulSoup
 
 def clear_data(data: list) -> list:
+    """
+    Очищает HTML-теги из списка строк.
+
+    Параметры:
+    data (list): Список строк, содержащий HTML-теги.
+
+    Возвращает:
+    list: Список строк без HTML-тегов.
+    """
     cleaned_data = []
     for item in data:
         if isinstance(item, str):  
@@ -24,10 +33,27 @@ def clear_data(data: list) -> list:
             
 class Scrap():
     def __init__(self):
+        """
+        Инициализирует экземпляр класса Scrap.
+        """
         pass
-    def add_url(self, url):
+
+    def add_url(self, url: str):
+        """
+        Добавляет URL в экземпляр Scrap.
+
+        Параметры:
+        url (str): URL для добавления.
+        """
         self.start = BeautifulSoup(url, 'lxml.parser')
-    def parse(self):
+
+    def parse(self) -> list:
+        """
+        Парсит HTML-содержимое добавленного URL и извлекает данные о вакансиях.
+
+        Возвращает:
+        list: Список, содержащий названия вакансий, описания, зарплаты и URL-адреса.
+        """
         titles = []
         descriptions = []
         salary = []
@@ -43,4 +69,3 @@ class Scrap():
         data_set.append(salary)
         data_set.append(urls)
         return data_set
-            
